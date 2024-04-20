4ffcac4c5a798875702adff5f4e5ec1115225b55
Ghazaleh Shahrokhabadi <gh.nshahrokhi@gmail.com>
2024-04-20 16:55:00 +0330

    Update ShahabController.java

--- src/main/java/ir/co/sadad/cheque/web/rest/ShahabController.java
+++ src/main/java/ir/co/sadad/cheque/web/rest/ShahabController.java
@@ -18,34 +18,9 @@ import javax.validation.Valid;
 
 
 @RestController
-@RequestMapping("/api/shahab")
 @RequiredArgsConstructor
 @Log4j2
 public class ShahabController {
 
-    private final ShahabService shahabService;
 
-    @PostMapping("/inquiry")
-    @PreAuthorize("hasAuthority('icms-nahab-inquiry')")
-    public ResponseEntity<ShahabSuccessResponseDto> inquiryRequest(@Valid @RequestBody ShahabRequestDto shahabRequestDto) {
-        return new ResponseEntity<>(shahabService.inquiryRequest(shahabRequestDto), HttpStatus.OK);
-
-    }
-
-    @GetMapping("/track")
-    @PreAuthorize("hasAuthority('icms-nahab-inquiry')")
-    public ResponseEntity<ShahabSuccessResponseDto> trackRequest(
-        @RequestParam("trackCode") String trackCode,
-        @RequestParam("ssn") String ssn) {
-        return new ResponseEntity<>(shahabService.trackRequest(ssn, trackCode), HttpStatus.OK);
-    }
-
-    @PostMapping("/code")
-    @Operation(summary = "سرویس نمایش کد شهاب", description = "سرویس نمایش کد شهاب مشتریان حقیقی و حقوقی ایرانی و اتباع بیگانه V1")
-    @ApiResponse(responseCode = "200",
-        content = @Content(schema = @Schema(implementation = ShahabSuccessResponseDto.class)))
-    @PreAuthorize("hasAuthority('SCOPE_account-super')")
-    public ResponseEntity<ShahabSuccessResponseDto> codeRequest(@RequestParam("ssn") String ssn) {
-        return new ResponseEntity<>(shahabService.codeRequest(ssn), HttpStatus.OK);
-    }
 }
